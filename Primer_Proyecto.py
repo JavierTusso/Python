a29d8c43b0b4c2b297007ba840bcde6e26ef84d6
javier.grinan.cano <javier.grinan.cano.contractor@bbva.com>
2024-11-07 18:22:34 +0100

    [Python] - First project

--- Primer_Proyecto.py
+++ Primer_Proyecto.py
@@ -1,1 +1,51 @@
 print("Hola Mundo")
+print("Hola" + " " + "\"Javi\"")
+
+###################
+
+print("Tu nombre es " + input("Dime tu nombre: ") + " " + input("Dime tu apellido: "))
+
+#Práctica Print 1
+
+print("Me encanta estudiar Python")
+
+#Práctica Print 2
+
+print('Estudiar con "Python Total" es super divertido')
+
+#Práctica Print 3
+
+print(5+550)
+
+#Práctica String 1 Crea un código que imprima en pantalla la siguiente expresión (pero usando print una sola vez):
+
+
+print("Línea 1\nLínea 2\nLínea 3")
+
+#Práctica String 2 Crea un código que imprima en pantalla la siguiente expresión.
+
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+
+#Práctica String 3
+
+print("Barra Normal: /\nBarra Invertida: \\")
+
+#Práctica Input 1
+
+print(input("¿Qué estás estudiando?"))
+
+#Práctica Input 2
+
+print(input("¿En qué país vives?"))
+
+#Práctica Input 3
+
+print(input("Escribe tu nombre:")+" "+input("Escribe tu apellido:"))
+
+
+
+################### PRIMER PROYECTO ###################
+# Pide al usuario que responda a dos preguntas que le generarán la combinación que será una marca creativa.
+p1 = input("¿Qué podemos ver en el cielo por la noche? ")
+p2 = input("¿Cómo se denomina la parte derecha de la península ibérica? ")
+print("La marca de tu cerveza será llamada: '" + p1 + " " + p2 +"'\n ¡Felicitaciones!")
